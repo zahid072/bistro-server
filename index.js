55d38546c875cfe161caf156041d2b0fef09f06e
zahid072 <jahidhasan19368@gmail.com>
2024-05-25 19:51:15 +0600

    user post and get apis created

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ async function run() {
     const myCartCollection = client
       .db("bistroRestaurantDB")
       .collection("myCart");
+    const allUsersCollection = client
+      .db("bistroRestaurantDB")
+      .collection("allUsers");
+    //  all apis
 
+    // ----------------------------------get apis----------------------------------
     app.get("/allMenu", async (req, res) => {
       const menu = await menuCollection.find().toArray();
       res.send(menu);
@@ -48,12 +53,25 @@ async function run() {
       const result = await myCartCollection.find(filter).toArray();
       res.send(result);
     });
-    app.post("/myCart", async (req, res) => { 
+    app.get("/allUsers", async (req, res) => {
+      const result = await myCartCollection.find().toArray();
+      res.send(result);
+    });
+
+    // -------------------------post apis-------------------------
+
+    app.post("/myCart", async (req, res) => {
       const newMenu = req.body;
       const result = await myCartCollection.insertOne(newMenu);
       res.send(result);
     });
+    app.post("/allUsers", async (req, res) => {
+      const newUser = req.body;
+      const result = await allUsersCollection.insertOne(newUser);
+      res.send(result);
+    });
 
+    // --------------------------update apis--------------------------
     app.patch("/myCart/:id", async (req, res) => {
       const { id } = req.params;
       const filter = { menuId: id };
@@ -63,10 +81,11 @@ async function run() {
           quantity: updatedValue.quantity,
         },
       };
-      const result =await myCartCollection.updateOne(filter, updatedDoc);
+      const result = await myCartCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
 
+    // ------------------------------delete apis------------------------------
     app.delete("/myCart/:id", async (req, res) => {
       const { id } = req.params;
       const filter = { _id: new ObjectId(id) };
